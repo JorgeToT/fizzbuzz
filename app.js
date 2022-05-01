2807f5971ad3922cb8369d402c1b6d02bb066f89
Jorge Aguilar <jorge.agvel@gmail.com>
2022-05-01 00:33:15 -0500

    Add fizzbuzz service to app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// Clase anterior con la que obtenemos los explorers
 const Reader = require("./lib/utils/Reader");
 const explorers = Reader.readJsonFile("explorers.json");
 
-// Aplicación del ExplorerService sobre la lista de explorers
 const ExplorerService = require("./lib/services/ExplorerService");
 ExplorerService.filterByMission(explorers, "node");
 ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 const names = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+//console.log(names);
 
-console.log(names);
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
+const tricks = explorers.map((explorer) =>
+  FizzbuzzService.applyValidationInExplorer(explorer)
+);
+// console.log(tricks);
