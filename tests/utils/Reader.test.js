ebe720a091dfd95ee1fcdbb92db287cdabbbb134
Jorge Aguilar <jorge.agvel@gmail.com>
2022-04-30 23:24:40 -0500

    Add tests for Explorer Services and Json file to tests

--- tests/utils/Reader.test.js
+++ tests/utils/Reader.test.js
@@ -1,9 +1,9 @@
-const readJsonFile = require("../lib/utils/Reader");
+const Reader = require("../../lib/utils/Reader");
 
 describe("Reader works", () => {
   test("readJsonFile returns an array", () => {
     const filePath = "explorers.json";
-    const explorers = readJsonFile.readJsonFile(filePath);
+    const explorers = Reader.readJsonFile(filePath);
     expect(explorers).toBeInstanceOf(Array);
   });
 });
