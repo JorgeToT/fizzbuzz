c6748aa75d8eeabc732141917822f2985478c3e0
Jorge Aguilar <jorge.agvel@gmail.com>
2022-05-01 21:35:34 -0500

    Create test for explorer controller username by mission and amount of explorers by mission

--- tests/controllers/ExplorerController.test.js
+++ tests/controllers/ExplorerController.test.js
@@ -4,4 +4,12 @@ describe ("Test for Explorer Controller",() => {
         const explorersFilter = ExplorerController.getExplorersByMission("node");
         expect(explorersFilter[0].name).toContain("Woopa1");
     });
+    test ("Get explorers user name by mission", () => {
+        const explorersFilterUsername = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorersFilterUsername).toContain("ajolonauta1");
+    });
+    test ("Get explorers amount by mission" , () => {
+        const explorersAmount = ExplorerController.getExplorersAmountByMission("node");
+        expect(explorersAmount).toBe(10);
+    });
 });
